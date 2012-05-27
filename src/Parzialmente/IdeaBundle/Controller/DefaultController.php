2a9d5d8a6a38633ea9d94ab7c05da3dd7d65cbdd
Giacomo Ritucci <giacomo.ritucci@email.it>
2012-05-28 00:55:12 +0200

    Controller e view di prova per mostra e nuova

--- src/Parzialmente/IdeaBundle/Controller/DefaultController.php
+++ src/Parzialmente/IdeaBundle/Controller/DefaultController.php
@@ -8,8 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+    public function nuovaAction()
     {
-        return $this->render('ParzialmenteIdeaBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ParzialmenteIdeaBundle:Default:nuova.html.twig', array());
+    }
+
+    public function mostraAction($id, $slug)
+    {
+	$idea = array("id" => $id, "description" => "TODO carica da DB l'idea");
+        return $this->render('ParzialmenteIdeaBundle:Default:idea.html.twig', array("idea" => $idea));
     }
 }
