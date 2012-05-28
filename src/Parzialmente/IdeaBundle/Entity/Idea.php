f49fae7b608c581cc80188c8eb99db631dc177dc
Giacomo Ritucci <giacomo.ritucci@email.it>
2012-05-29 00:40:11 +0200

    Generata entity Idea
    php app/console doctrine:generate:entity          \
        --entity=ParzialmenteIdeaBundle:Idea          \
        --fields=title:string(255) description:text

---
+++ src/Parzialmente/IdeaBundle/Entity/Idea.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Parzialmente\IdeaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Parzialmente\IdeaBundle\Entity\Idea
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Idea
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var text $description
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param text $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Get description
+     *
+     * @return text 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
