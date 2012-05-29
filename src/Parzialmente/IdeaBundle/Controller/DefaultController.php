607969c3529b0b2d0f2d0c8aac00be2a6de0fc74
Giacomo Ritucci <giacomo.ritucci@email.it>
2012-05-29 21:57:33 +0200

    Form nuova idea implementata nel Controller

--- src/Parzialmente/IdeaBundle/Controller/DefaultController.php
+++ src/Parzialmente/IdeaBundle/Controller/DefaultController.php
@@ -3,19 +3,36 @@
 namespace Parzialmente\IdeaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Parzialmente\IdeaBundle\Entity\Idea;
 
 
 class DefaultController extends Controller
 {
-    
+
     public function nuovaAction()
     {
-        return $this->render('ParzialmenteIdeaBundle:Default:nuova.html.twig', array());
+        $idea = new Idea();
+        $idea->setTitle("Dai un titolo");
+        $idea->setDescription("Descrivi la tua idea.");
+
+        $form = $this->createFormBuilder($idea)
+            ->add("title", "text")
+            ->add("description", "textarea")
+            ->getForm();
+
+        return $this->render('ParzialmenteIdeaBundle:Default:nuova.html.twig', array(
+            "form" => $form->createView()
+        ));
     }
 
     public function mostraAction($id, $slug)
     {
-	$idea = array("id" => $id, "description" => "TODO carica da DB l'idea");
+        $idea = array("id" => $id, "description" => "TODO carica da DB l'idea");
         return $this->render('ParzialmenteIdeaBundle:Default:idea.html.twig', array("idea" => $idea));
+    }
+
+
+    public function ideeAction(Request $request)
+    {
     }
 }
